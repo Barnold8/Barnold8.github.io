fa8600c7e1e72ee2063c35791cfac903a6c5da09
Brandon Wright <bandslover@outlook.com>
2020-10-27 19:56:10 +0000

    N/A

--- Home/js/index.js
+++ Home/js/index.js
@@ -16,3 +16,9 @@
 //});
 
 
+var siteWidth = 1280;
+var scale = screen.width /siteWidth;
+
+document.querySelector('meta[name="viewport"]').setAttribute('content', 'width='+siteWidth+', initial-scale='+scale+'');
+
+
