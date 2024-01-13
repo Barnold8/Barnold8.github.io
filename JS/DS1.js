45f1a82e327accfc52ffa0b15e521f8b34840328
Brandon Wright <bandslover@outlook.com>
2024-01-13 18:27:13 +0000

    Made the calculator actually perform an operation the game doesnt...

--- JS/DS1.js
+++ JS/DS1.js
@@ -7,15 +7,23 @@ function soulCalc() {
 
     let numberPattern = /[0-9]+(\.[0-9]+)?/g; // this regex also includes floats but i forgot i only needed integers. Doesnt effect speed so we are fine
     
+    let currentSoulLevelInput  = document.getElementById("currentSoulLevel").value;
     let desiredSoulLevelInput  = document.getElementById("desiredSoulLevel").value;
-   
     let soulsNeeded            = document.getElementById("soulsNeeded");
 
+    let currentSoulLevelMatch = currentSoulLevelInput.match(numberPattern);
     let desiredSoulLevelMatch = desiredSoulLevelInput.match(numberPattern);
- 
+
+    let currentSoulLevel = parseInt(currentSoulLevelMatch.join(''))
     let desiredSoulLevel = parseInt(desiredSoulLevelMatch.join(''))
-   
-    let soulsTo = parseInt((0.02 * (desiredSoulLevel ** 3)) + (3.06 * (desiredSoulLevel ** 2)) + ((105.6 * desiredSoulLevel) - 895)); // i don't know why, but I just dont trust precedence  
-  
-    soulsNeeded.innerText = `You need ${soulsTo} souls to level up`
+
+    let souls = 0;
+
+    for(let i = 0; i < desiredSoulLevel - currentSoulLevel; i++){
+        // i don't know why, but I just dont trust precedence  
+        souls += parseInt((0.02 * ((currentSoulLevel + i) ** 3)) + (3.06 * ((currentSoulLevel + i) ** 2)) + ((105.6 * (currentSoulLevel + i)) - 895));
+        
+    }
+
+    soulsNeeded.innerText = `You need ${souls} souls`
   }
