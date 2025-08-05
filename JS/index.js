851540f48f8e4a736e886b29a925a410c93f5814
Barnold88 <bandslover@outlook.com>
2025-08-05 02:32:48 +0100

    Add cards explaining relevent projects

--- JS/index.js
+++ JS/index.js
@@ -1,14 +1,17 @@
-const hoverText = document.querySelector('.hover-text');
+const hoverTexts = document.querySelectorAll('.hover-text');
 const hoverCard = document.getElementById('hoverCard');
 
-hoverText.addEventListener('mousemove', (e) => {
-const content = hoverText.getAttribute('data-info');
-hoverCard.textContent = content;
-hoverCard.style.left = `${e.clientX + 10}px`; // add some offset
-hoverCard.style.top = `${e.clientY + 10}px`;
-hoverCard.style.display = 'block';
-});
+hoverTexts.forEach((hoverText) => {
+  hoverText.addEventListener('mousemove', (e) => {
+    const content = hoverText.getAttribute('data-info');
+    const formatted = content.replace(/(\r\n|\n|\r|&#10;|&#13;)/g, '<br>');
+    hoverCard.innerHTML = formatted; 
+    hoverCard.style.left = `${e.clientX + 10}px`; 
+    hoverCard.style.top = `${e.clientY + 10}px`;
+    hoverCard.style.display = 'block';
+  });
 
-hoverText.addEventListener('mouseleave', () => {
-hoverCard.style.display = 'none';
+  hoverText.addEventListener('mouseleave', () => {
+    hoverCard.style.display = 'none';
+  });
 });
