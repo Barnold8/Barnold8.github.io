35202e87951560eb4d2ef84a630ff3aa0e778d60
Barnold88 <bandslover@outlook.com>
2025-08-05 03:13:27 +0100

    Add carousel of icons that shouldve been done with JS generating the HTML but oh well

--- JS/index.js
+++ JS/index.js
@@ -15,3 +15,18 @@ hoverTexts.forEach((hoverText) => {
     hoverCard.style.display = 'none';
   });
 });
+
+
+// cos im lazy im adding class to all slide elements here after dom load
+
+
+slides = document.getElementsByClassName("slide")
+// two for loops but its HIGHLY unlikely ill be adding sub images in here, 
+// its more a precautionary measure that theres some img before the actual one thats non existant and it messes it all up
+for (let elem of slides) {
+  images = elem.getElementsByTagName("img")
+  for (let img of images) {
+     img.classList.add('slideImg')
+  }
+}
+
